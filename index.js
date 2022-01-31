1d0df6043f5184cc7c918fb35bcb1a52b225f665
Ellen Treidler <ellentreidler@gmail.com>
2022-01-31 14:30:00 +0200

    JS weather forecast

--- index.js
+++ index.js
@@ -65,30 +65,42 @@ function showTemperature(response) {
     showDetails(response)
 }
 
+
 function displayForecast() {
     let forecastElement = document.querySelector("#forecast")
 
-    forecastElement.innerHTML = `
-    <div className = "weather-forecast-right" id = "forecast" >
-        <h2> Forecast </h2>
-    <hr className="newColor">
-        <div className="container">
-            <div className="row">
-                <div className="col-">Mon</div>
-                <img
-                    src="https://cdn.iconscout.com/icon/free/png-256/weather-2191838-1846632.png"
-                    alt=""
-                    width="32px"
-                />
-                <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max"> 18° </span>
-                    <span className="weather-forecast-temperature-min"> 12° </span>
-                </div>
-            </div>
-        </div>
-    </div>
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = `<div class="row">`;
+
+    days.forEach(function (day) {
+
+        forecastHTML = forecastHTML + `
+
+                            <div class="col-2">
+                            <div class="weather-forecast-date">${day}</div>
+                            <img
+                                src="https://cdn.iconscout.com/icon/free/png-256/weather-2191838-1846632.png"
+                                alt=""
+                                width="32px"
+                            />
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max"> 18° </span>
+                                <span class="weather-forecast-temperature-min"> 12° </span>
+                            </div>
+                    </div>
+          
     `;
+
+
+    })
+
+    forecastHTML = forecastHTML + `</div>`;
+
+    forecastElement.innerHTML = forecastHTML;
+
 }
+
 
 function units() {
     if (temperatureButtonC.checked) {
@@ -155,7 +167,7 @@ function currentFormSubmit(event) {
 function unitsChange() {
     if (lastCity) {
         updateWeather(lastCity)
-    }else {
+    } else {
         navigator.geolocation.getCurrentPosition(showPosition)
     }
 }
@@ -175,7 +187,6 @@ let fButton = document.querySelector("#btnradio2")
 fButton.addEventListener("click", unitsChange)
 
 updateWeather(lastCity)
-
 displayForecast();
 
 
